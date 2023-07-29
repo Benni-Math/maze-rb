6af20a6891220d5ec299314ddf663d67b841d79f
Benni-Math <benediktjens.arnarsson@gmail.com>
2023-07-28 21:19:52 -0400

    Initial commit

---
+++ lib/maze.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "maze/version"
+
+module Maze
+  class Error < StandardError; end
+  # Your code goes here...
+  def hello
+    puts 'hello world'
+  end
+end
---
+++ lib/maze/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Maze
+  VERSION = "0.1.0"
+end
